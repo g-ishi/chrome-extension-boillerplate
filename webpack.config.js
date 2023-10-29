b1a09dc1c9b6f9ee5d93c94d57d81a467ef559bb
Genichi-Ishii <Genichi.Ishii@ibm.com>
2023-10-29 15:06:52 +0900

    feat: backgroundとcontentScriptの設定追加

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,8 @@ module.exports = {
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
     options: path.resolve('src/options/options.tsx'),
+    background: path.resolve('src/background/background.ts'),
+    contentScript: path.resolve('src/contentScript/contentScript.ts'),
   },
   // バンドルするときの追加ルールを定義する
   module: {
